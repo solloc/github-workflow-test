cbb8bd6d954f84812ebb7b8807293f33ca38b910
solloc <matthias@maisch.me>
2022-09-25 19:06:16 +0200

    express implemented

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const http = require("http");
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server");
-}
-const server = http.createServer(requestListener);
-server.listen(8080, "localhost", () => {
-    console.log(`Server is running on http://localhost:8080`);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
